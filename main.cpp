95d73088324356da6c53ee00b84bc6267ce24228
Lorenzo <lorenzo.millucci@outlook.com>
2013-11-04 20:35:12 +0100

    migliorato sistema erode/dilate

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int V_MAX = 256;
 const string mainGui="Immagine acquisita";
 const string thresholdWindow="Immagine rilevata";
 const string settingWindow="Imposta soglia";
-const string blurWindow="Immagin con filtro Blur";
+const string blurWindow="Immagine con filtro Blur";
 
 //action listener per gli slider -> INUTILISSIMO MA NECESSARIO
 void onTrackbarSlide(int, void*){
@@ -35,8 +35,8 @@ void createSlider(){
     createTrackbar("S-min",settingWindow, &S_MIN, S_MAX,onTrackbarSlide);
     createTrackbar("V-min",settingWindow, &V_MIN, V_MAX,onTrackbarSlide);
     createTrackbar("H-max",settingWindow, &H_MAX, H_MAX, onTrackbarSlide);
-   createTrackbar("S-max",settingWindow, &S_MAX, S_MAX,onTrackbarSlide);
-   createTrackbar("V-max",settingWindow, &V_MAX, V_MAX,onTrackbarSlide);
+    createTrackbar("S-max",settingWindow, &S_MAX, S_MAX,onTrackbarSlide);
+    createTrackbar("V-max",settingWindow, &V_MAX, V_MAX,onTrackbarSlide);
 }
 
 
@@ -68,32 +68,35 @@ int main(int argc,char* argv[]){
 		//catturo un frame della webcam
 		capture.read(cameraFeed);
 		
-
 		//applico il gaussian blur
 		medianBlur(cameraFeed,frameBlur,5);
 
 		//cambio lo spazio dei colori RGB-->HSV
 		//cvtColor(sorgente, destinazione, operazione)
 		cvtColor(frameBlur,hsvFrame,COLOR_BGR2HSV);
-
-
 		
 		//filtro hsvFrame cercando solo un determinato range di colori
 		//void inRange(InputArray src, InputArray lowerbound, InputArray upperbound, OutputArray dst)
 		inRange(hsvFrame, Scalar(H_MIN,S_MIN,V_MIN),Scalar(H_MAX,S_MAX,V_MAX),thresholded);
 		
-		//applico l'erosione
-		//erode(InputArray src, OutputArray dst, InputArray kernel, Point anchor=Point(-1,-1), int iterations=1,
-		//int borderType=BORDER_CONSTANT, const Scalar& borderValue=morphologyDefaultBorderValue() )
-		erode(thresholded, thresholded, 0, Point(-1, -1), 2, 1, 1);
-		dilate(thresholded, thresholded, 0, Point(-1, -1), 2, 1, 1);
+		//--------applico l'erosione e la dilatazione---------
+		//questa matrice  indica che i 3x3 pixel intorno ad ogni punto da analizzare sono un rettangolo
+		//in pratica per ogni pixel erodo il rettangolo 3x3 intorno 
+		//IN PRATICA: con l'erosione rimuovo tutti i puntini piccoli che compaiono
+		Mat rectErosione = getStructuringElement(MORPH_RECT,Size(3,3));
+		erode(thresholded, thresholded,rectErosione);
+		erode(thresholded, thresholded,rectErosione);
+		//dilato ogni pixel rilevato in un rect 8x8
+		Mat rectDilataz = getStructuringElement( MORPH_RECT,Size(8,8));
+		dilate(thresholded, thresholded, rectDilataz);
+		dilate(thresholded, thresholded, rectDilataz);
 		
 		//visualizzo su mainGui il frame originale
 		imshow(mainGui,cameraFeed);
 		//visualizzo su thresholdWindow l'immagine filtrata
 		imshow(thresholdWindow,thresholded);
 		//visualizzo su blurWindow l'immagine con il filtro blur
-		imshow(blurWindow,frameBlur);
+		//imshow(blurWindow,frameBlur);
 		
 		//premi un pulsante per uscire oppure attendi il tempo di attesa prima di passare al frame successivo
 		if(waitKey(33) >= 0) break;
